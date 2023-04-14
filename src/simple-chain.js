e2b69c993b8e60e90e2b4e2d5aec54d22db47c10
Nikita Gromov <nikita.gromoff@gmail.com>
2023-04-14 08:59:02 +0300

    fix: remove git pull comments

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -17,18 +17,10 @@ const { NotImplementedError } = require('../extensions/index.js');
     return this;
   },
   removeLink(n) {
-<<<<<<< HEAD
-    if (n < 1 || n > this.chain.length || !Number.isInteger(n) ) {
-           
-      this.chain = [];
-      throw new Error ('You can\'t remove incorrect link!');
-        } 
-        
-        this.chain.splice(n-1,1);
-=======
+
     if (n-1 < 0 || n-1 > this.chain.length-1 || isNaN(n)) throw new Error (`You can't remove incorrect link!`);
     this.chain.splice(n-1,1);
->>>>>>> 5114711389963506e6b78decdfd0ef8a1da4e2be
+
     return this;
   },
   reverseChain() {
